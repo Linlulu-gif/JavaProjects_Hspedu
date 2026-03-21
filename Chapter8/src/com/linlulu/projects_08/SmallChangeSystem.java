b6be7d236065d1f0832c41a74bfe414f448e1794
Linlulu-gif <646603813@qq.com>
2026-03-21 10:17:03 +0800

    update

--- Chapter8/src/com/linlulu/projects_08/SmallChangeSystem.java
+++ Chapter8/src/com/linlulu/projects_08/SmallChangeSystem.java
@@ -1,5 +1,7 @@
 package com.linlulu.projects_08;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 //在这里实现   零钱通  菜单显示
@@ -7,27 +9,42 @@ public class SmallChangeSystem {
     public static void main(String[] args) {
         /*
             化繁为简
-            1. 将需求中的菜单一步步搭建出来，能够完成基本的选择某个模块的功能
+            将需求中的菜单一步步搭建出来，能够完成基本的选择某个模块的功能
+            1. 先完成菜单显示
+            2. 完成零钱通明细
+            （1）使用数组保存 入账信息 和 消费；（2）使用对象； （3）使用字符串拼接 sout即可
+            3. 完成收益入账
         */
         boolean flag = true;// 进入系统否
         Scanner scanner = new Scanner(System.in);
-
+        //  1. 先完成菜单显示
         do {
-            System.out.println("********************零钱通菜单************************");
+            System.out.println("\n********************零钱通菜单************************");
             System.out.println("\t\t\t\t1 零钱通明细\t\t\t\t");
             System.out.println("\t\t\t\t2 收益 入账\t\t\t\t");
             System.out.println("\t\t\t\t3 消    费\t\t\t\t");
             System.out.println("\t\t\t\t4 退出 系统\t\t\t\t");
-            System.out.print("请选择(1-4): \t");
+            System.out.print("请选择(1-4): ");
             String choose = scanner.next();
+            // 2. 完成零钱通明细
+            String details = "********************零钱通明细************************";
+
+            // 3. 完成收益入账
+            double inMoney;//入账
+            Date date;//日期
+            double balance = 0;//余额
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd  hh:mm:ss");
 
             //使用switch结构完成多个模块的业务跳转
-            switch (choose){
+            switch (choose) {
                 case "1":
-                    System.out.println("********************零钱通明细************************");
+                    System.out.println(details);//完成
                     break;
                 case "2":
-                    System.out.println("********************收益 入账************************");
+                    inMoney = scanner.nextDouble();
+                    balance += inMoney;
+                    date = new Date();//获取当前时间
+                    details += "\n收益入账   +" + inMoney + "\t" + dateFormat.format(date) + "\t" + balance;
                     break;
                 case "3":
                     System.out.println("********************消    费************************");
@@ -37,6 +54,7 @@ public class SmallChangeSystem {
                     flag = false;
                     break;
                 default:
+                    System.out.println("选择有误，请重新选择！！！");
             }
         } while (flag);
 
