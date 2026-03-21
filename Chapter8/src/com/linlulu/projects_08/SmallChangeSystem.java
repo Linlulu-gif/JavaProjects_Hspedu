d890fe151ff4bfccd1d9980b791bbc298cc06f90
Linlulu-gif <646603813@qq.com>
2026-03-21 10:49:37 +0800

    update

--- Chapter8/src/com/linlulu/projects_08/SmallChangeSystem.java
+++ Chapter8/src/com/linlulu/projects_08/SmallChangeSystem.java
@@ -15,9 +15,21 @@ public class SmallChangeSystem {
             （1）使用数组保存 入账信息 和 消费；（2）使用对象； （3）使用字符串拼接 sout即可
             3. 完成收益入账
         */
-        boolean flag = true;// 进入系统否
+        boolean flag = true;// 控制系统运行
         Scanner scanner = new Scanner(System.in);
-        //  1. 先完成菜单显示
+        // 1. 先完成菜单显示
+        // 2. 完成零钱通明细
+        String details = "********************零钱通明细************************";
+
+        // 3. 完成收益入账
+        double inMoney;//入账
+        double outMoney;//消费金额
+        String note;//消费说明
+        Date date;//日期
+        double balance = 0;//余额
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd  HH:mm:ss");//HH:mm:ss
+        // 使用24小时制，避免12小时制混淆
+
         do {
             System.out.println("\n********************零钱通菜单************************");
             System.out.println("\t\t\t\t1 零钱通明细\t\t\t\t");
@@ -26,14 +38,6 @@ public class SmallChangeSystem {
             System.out.println("\t\t\t\t4 退出 系统\t\t\t\t");
             System.out.print("请选择(1-4): ");
             String choose = scanner.next();
-            // 2. 完成零钱通明细
-            String details = "********************零钱通明细************************";
-
-            // 3. 完成收益入账
-            double inMoney;//入账
-            Date date;//日期
-            double balance = 0;//余额
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd  hh:mm:ss");
 
             //使用switch结构完成多个模块的业务跳转
             switch (choose) {
@@ -41,13 +45,30 @@ public class SmallChangeSystem {
                     System.out.println(details);//完成
                     break;
                 case "2":
+                    System.out.println("请输入入账金额：");
                     inMoney = scanner.nextDouble();
+                    if (inMoney <= 0) {
+                        System.out.println("入账金额输入错误！！！！！");
+                        break;
+                    }
                     balance += inMoney;
                     date = new Date();//获取当前时间
-                    details += "\n收益入账   +" + inMoney + "\t" + dateFormat.format(date) + "\t" + balance;
+                    details += ("\n收益入账\t+" + inMoney + "\t" + dateFormat.format(date) + "\t" + balance);
+                    System.out.println("入账成功（-_-)");
                     break;
                 case "3":
-                    System.out.println("********************消    费************************");
+                    System.out.println("输入消费金额：");
+                    outMoney = scanner.nextDouble();
+                    if (outMoney <= 0 || outMoney > balance) {
+                        System.out.println("消费金额无效 或 超出了当前余额");
+                        break;
+                    }
+                    System.out.println("输入消费说明：");
+                    note = scanner.next();
+                    balance -= outMoney;
+                    date = new Date();//获取当前时间
+                    details += ("\n" + note + "\t-" + outMoney + "\t" + dateFormat.format(date) + "\t" + balance);
+                    System.out.println("消费成功（>_<)");
                     break;
                 case "4":
                     System.out.println("********************退出 系统************************");
